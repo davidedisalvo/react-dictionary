a3070e9d5288205e206458abf45a095e73be600a
davide <davidesimone.disalvo@gmail.com>
2020-04-15 17:24:52 +0100

    handle not fav word error

--- src/components/TheNav.js
+++ src/components/TheNav.js
@@ -1,32 +1,46 @@
-import React, { Component, useContext } from 'react';
-import {Navbar, Nav} from 'react-bootstrap';
-import {Link} from 'react-router-dom';
-import WordContext from './WordsContext';
+import React, { Component, useContext } from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
+import WordContext from "./WordsContext";
 
- 
 class TheNav extends Component {
-    static contextType = WordContext
+  static contextType = WordContext;
 
-    render() { 
-        const value = this.context
+  render() {
+    const value = this.context;
 
-        return (
-            <Navbar bg="light" expand="lg">
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        
-                        <Link to="/" className= "nav-link">Home</Link>
-                        <Link to="/words" className="nav-link">
-                            My Words ({value.favourite.length})
-                        </Link>
-                    
-                    </Nav>
-                    
-                </Navbar.Collapse>
-            </Navbar>
-        );
-    }
+    return (
+      <Navbar bg="light" expand="lg">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <NavLink
+              to="/"
+              exact
+              activeStyle={{
+                fontWeight: "bold",
+                color: "#173f5f",
+              }}
+              className="nav-link"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/words"
+              className="nav-link"
+              exact
+              activeStyle={{
+                fontWeight: "bold",
+                color: "#173f5f",
+              }}
+            >
+              My Words ({value.favourite.length})
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
 }
- 
+
 export default TheNav;
--- src/components/WordsList.js
+++ src/components/WordsList.js
@@ -1,40 +1,53 @@
-import React, { Component } from 'react';
-import SingleWord from './SingleWord';
-import WordContext from './WordsContext';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import React, { Component } from "react";
+import SingleWord from "./SingleWord";
+import WordContext from "./WordsContext";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Alert from "react-bootstrap/Alert";
 
- 
 class WordsList extends Component {
-    static contextType = WordContext
-    handleDelete = (index) => {
-        const { definition, setDefinition,  saveWord, deleteSavedWord} = this.context
-        deleteSavedWord(index)
-    }
-    render() {
-        const value = this.context
-        return (
-            <Container>
-                <Row>
-                    <Col>
-            <div>     
-                {value.favourite ? 
-                    value.favourite.map((item, index) => 
-                        <SingleWord 
-                            title={item.title}
-                            index={index}
-                            definition={item.definition} 
-                            delete={this.handleDelete}>
-                        </SingleWord>) 
-                : ''}
-            </div>
+  static contextType = WordContext;
+  handleDelete = (index) => {
+    const {
+      definition,
+      setDefinition,
+      saveWord,
+      deleteSavedWord,
+    } = this.context;
+    deleteSavedWord(index);
+  };
+  render() {
+    const value = this.context;
+    return (
+      <Container>
+        {value.favourite.length !== 0 ? (
+          <Row>
+            <Col>
+              <div>
+                {value.favourite
+                  ? value.favourite.map((item, index) => (
+                      <SingleWord
+                        title={item.title}
+                        index={index}
+                        definition={item.definition}
+                        delete={this.handleDelete}
+                      ></SingleWord>
+                    ))
+                  : ""}
+              </div>
             </Col>
-            </Row>
-            </Container>
-
-        );
-    }
+          </Row>
+        ) : (
+          <Col>
+            <Alert variant="danger">
+              You don't have any favourite word yet
+            </Alert>
+          </Col>
+        )}
+      </Container>
+    );
+  }
 }
- 
+
 export default WordsList;
